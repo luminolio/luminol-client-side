549239a0d161411176871ca04a70998b0518d8aa
Daniel de Andrade Varela <daniel@macaco-cerebro.localdomain>
2016-06-19 20:59:03 -0300

    init

---
+++ luminol/luminol.components.ts
@@ -0,0 +1,22 @@
+enum LuminolComponentLoadType {
+
+}
+
+export class LuminolComponent {
+	constructor() 
+	{
+
+	}
+
+	private _load() 
+	{}
+
+	private _loadStyle() 
+	{}
+
+	private _loadScript() 
+	{}
+
+	private _loadTemplate() 
+	{}
+}
